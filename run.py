9f1a7876c271c1e68cf841678b616c70b7b87fbe
Alexe Mihail <79512195+alexecse@users.noreply.github.com>
2025-04-03 21:35:49 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from group_htmls.grouping import group_similar_htmls
 
 if __name__ == "__main__":
-    # Rulează procesarea pe toate nivelurile specificate
     for tier in ['./clones/tier1', './clones/tier2', './clones/tier3', './clones/tier4']:
         print(f"Grouping for: {tier}")
         group_similar_htmls(tier, eps=2, min_samples=2, do_postprocessing=1)
